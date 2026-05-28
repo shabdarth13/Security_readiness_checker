e2e89037a159cca79bca3a062e059031a3adb086
SHABDARTH <shabdarth53@gmail.com>
2026-05-28 10:51:04 +0000

    commit

--- backend/config/init_db.py
+++ backend/config/init_db.py
@@ -3,7 +3,7 @@ from db import get_db_connection
 conn = get_db_connection()
 cur = conn.cursor()
 
-with open("database/schema.sql", "r") as file:
+with open("../database/schema.sql", "r") as file:
     sql = file.read()
 
 cur.execute(sql)
