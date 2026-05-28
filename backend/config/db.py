bfc7422c7653d7e44682a70074dd406f9f3243eb
SHABDARTH <shabdarth53@gmail.com>
2026-05-28 16:00:46 +0530

    commit 1234567890abcdef1234567890abcdef12345678

--- backend/config/db.py
+++ backend/config/db.py
@@ -6,18 +6,9 @@ from psycopg2.extras import RealDictCursor
 def get_db_connection():
     database_url = os.getenv("DATABASE_URL")
 
-    if database_url:
-        conn = psycopg2.connect(
-            database_url,
-            cursor_factory=RealDictCursor
-        )
-    else:
-        conn = psycopg2.connect(
-            host="localhost",
-            database="security_audit",
-            user="postgres",
-            password="7983434Abhi#",
-            cursor_factory=RealDictCursor
-        )
+    conn = psycopg2.connect(
+        database_url,
+        cursor_factory=RealDictCursor
+    )
 
     return conn
